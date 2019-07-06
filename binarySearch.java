53aaa476f8c1fcaca6db67f4335d52cf4ce6885a
Adnan Reza <adnan.reza@outlook.com>
2019-07-06 11:10:52 -0700

    Implemented recursive binary search

--- binarySearch.java
+++ binarySearch.java
@@ -1,11 +1,33 @@
-class Program {
+/*Binary Search*/
+
+class BinarySearch {
+
+	public static int binarySearch(int [] array, int target) {
+		return binarySearchR(int [] array, int target, 0, array.length -1);
+	}
+
 	/*Recursive binary search algorithm*/
-	public static int binarySearchR(int [] array, int target) {
-
+	public static int binarySearchR(int [] array, int target, int left, int right) {
+		//base case
+		if(left > right){ 
+			return -1;
+		}
+		//recursive case
+		int middle = (left + right) / 2;
+		if(target == array[middle]) {
+			return middle;
+		}
+		else if(target > array[middle]) {
+			return binarySearchR(array, target, middle+1, right);
+		}
+		else {
+			return binarySearchR(array, target, left, middle-1);
+		}
 	}
-	
+
 	/*Iterative binary search algorithm*/
-	public static int binarySearchI(int [] array, int target) {
-
+	public static int binarySearchI(int [] array, int target, int left, int right) {
+		
 	}
+
 }
