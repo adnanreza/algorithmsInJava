c641b900852dce71fe36e02c91b2d016264d9c45
Adnan Reza <adnan.reza@outlook.com>
2019-07-06 10:58:11 -0700

    Add binarSearch skeleton code

---
+++ binarySearch.java
@@ -0,0 +1,11 @@
+class Program {
+	/*Recursive binary search algorithm*/
+	public static int binarySearchR(int [] array, int target) {
+
+	}
+	
+	/*Iterative binary search algorithm*/
+	public static int binarySearchI(int [] array, int target) {
+
+	}
+}
