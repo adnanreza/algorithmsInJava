97eb485e459f15c6ba4376ffb02b6d3322e52360
Adnan Reza <adnan.reza@outlook.com>
2019-07-06 22:09:29 -0700

    Implemented Selection Sort

--- SortingAlgorithms.java
+++ SortingAlgorithms.java
@@ -1,7 +1,9 @@
 /*Sorting Algorithms in Java*/
 
 class SortingAlgorithms {
-	/*Bubble Sort
+
+	/*
+	  Bubble Sort
 	  Best case: O(n) time | O(1) space
 	  Average case: O(n^2) time | O(1) space
 	  Worst case: O(n^2) time | O(1) space
@@ -24,6 +26,29 @@ class SortingAlgorithms {
 		return array;
 	}
 
+	/*
+	  Selection Sort
+	*/
+	public static int[] selectionSort(int [] array) {
+		//Start index of unsorted list
+		int startIndex = 0;
+
+		while(startIndex < array.length - 1) {
+			int smallIndex = startIndex; //keep pointer of smallest index of unsorted list
+			for(int i=startIndex + 1; i<array.length; i++) {
+				if(array[i] < array[smallIndex]) {
+					smallIndex = i;
+				}
+			}
+			swap(array, smallIndex, startIndex);
+			startIndex++;
+		}
+		return array;
+	}
+
+	/*
+	  Helper subroutine: Swap
+	*/
 	public static void swap(int [] array, int a, int b) {
 		int temp = array[a];
 		array[a] = array[b];
